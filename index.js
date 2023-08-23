20e5549f0cb43b39f4b68a1d605919d3c19e8ff5
Replit user <>
2023-08-23 04:43:38 +0000

    Exercise tracker

--- index.js
+++ index.js
@@ -2,16 +2,101 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongo = require('./mongo')
 
 app.use(cors())
+app.use(express.json()); // for json
+app.use(express.urlencoded({ extended: true })); // for form data
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/users', async (req, res) => {
+  const data = await mongo.getData({ username: 1 })
+  res.json(data)
+});
 
+app.post('/api/users', async (req, res) => {
+  if (!req.body.username) {
+    res.json({ error: 'username is required' });
+    return;
+  }
 
+  const data = await mongo.saveData({
+    username: req.body.username
+  })
 
+  res.json({
+    _id: data.insertedId,
+    username: req.body.username
+  })
+});
+
+app.get('/api/users/:_id/logs', async (req, res) => {
+  if (!req.params._id) {
+    res.json('The user not found');
+    return;
+  }
+
+  const filter = { _id: mongo.toObjectId(req.params._id) };
+  const obj = { log: 1 };
+
+  if (req.query.from) {
+    filter['log.date'] = { $gte: req.query.from };
+  }
+
+  if (req.query.to) {
+    filter['log.date'] = { $lte: req.query.to };
+  }
+
+  if (req.query.limit && !isNaN(req.query.limit)) {
+    obj.log = { $slice: parseInt(req.query.limit) };
+  }
+
+  const data = await mongo.findData(filter, obj);
+
+  res.json({
+    count: data?.log ? data.log.length : 0,
+    log: data?.log ?? []
+  })
+});
+
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  if (!req.params._id) {
+    res.json('The user not found');
+    return;
+  }
+
+  if (!req.body.description) {
+    res.json('The description data invalid');
+    return;
+  }
+
+  if (!req.body.duration || isNaN(req.body.duration)) {
+    res.json('The duration data invalid');
+    return;
+  }
+
+  if (!req.body.date || !req.body.date.match(/^\d{4}-\d{2}-\d{2}$/)) {
+    res.json('The date data invalid');
+    return;
+  }
+
+  const logs = {
+    description: req.body.description,
+    duration: parseInt(req.body.duration),
+    date: req.body.date
+  }
+
+  const data = await mongo.updateData({
+    _id: mongo.toObjectId(req.params._id)
+  }, {
+    $addToSet: { log: logs }
+  });
+
+  res.json(data)
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
---
+++ mongo.js
@@ -0,0 +1,56 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const dbName = process.env.MONGO_DATABASE;
+const dbCollect = process.env.MONGO_COLLECTION;
+const client = new MongoClient(process.env.MONGO_URI);
+
+module.exports.saveData = async (obj) => {
+  try {
+    await client.connect();
+    const data = await client.db(dbName).collection(dbCollect).insertOne(obj);
+    client.close();
+    return data;
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+module.exports.updateData = async (filter = {}, obj = {}) => {
+  try {
+    await client.connect();
+    const data = await client.db(dbName).collection(dbCollect).updateOne(filter, obj);
+    client.close();
+    return data;
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+module.exports.getData = async (filter = {}, only = {}) => {
+  try {
+    await client.connect();
+    const data = await client.db(dbName).collection(dbCollect).find(filter).project(only).toArray();
+    client.close();
+    return data
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+module.exports.findData = async (filter = {}, only = {}) => {
+  try {
+    await client.connect();
+    const data = await client.db(dbName).collection(dbCollect).find(filter).project(only).limit(1).toArray();
+    client.close();
+    return data.length ? data[0] : null;
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+module.exports.toObjectId = (s) => {
+  if (!ObjectId.isValid(s)) {
+    throw new TypeError(`Invalid id: ${s}`);
+  }
+  
+  return new ObjectId(s);
+}
